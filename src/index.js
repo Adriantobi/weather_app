e88552efca610cba7a7151bc296a8b833d3fb1ff
Adrian Tobi <40503266+Adriantobi@users.noreply.github.com>
2024-03-19 10:39:33 +0000

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import './css/index.css';
+import Home from './pages/Home'
+import Airports from './pages/Airports'
+import Attractions from './pages/Attractions'
+import Nav from './components/nav'
+//import reportWebVitals from './reportWebVitals';
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="airports" element={<Airports />} />
+        <Route path="attractions" element={<Attractions />} />
+      </Routes>
+    </Router>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -14,4 +30,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//reportWebVitals();
